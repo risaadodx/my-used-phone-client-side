ff1f32cc685e5188da52e030e7fa6d9bee4f09e7
risaadodx <wrisaad@gmail.com>
2022-11-24 19:42:58 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div>
+      <footer className="footer p-10 bg-neutral text-neutral-content items-center">
+        <div>
+          <h2 className="text-3xl font-bold">Recycle Me</h2>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;

223d3f97a1a7e8812414a512f4aacdda7464b1f3
risaadodx <wrisaad@gmail.com>
2022-11-27 18:24:08 +0600

    user saving in db added

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -28,13 +28,29 @@ const Signup = () => {
         };
         updateUser(userInfo)
           .then(() => {
-            navigate("/");
+            saveUser(data.email, data.name);
           })
           .catch((e) => console.log(e));
       })
       .catch((error) => {
         console.error(error);
         setSignUpError(error.message);
+      });
+  };
+
+  const saveUser = (name, email) => {
+    const user = { name, email };
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        navigate("/");
       });
   };
   //Loading spinner//
